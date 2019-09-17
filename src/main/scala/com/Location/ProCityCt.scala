d2e7b961a4346a1d4105f1e6f83a782662a387b5
mg1225 <1403578051@qq.com>
2019-09-17 20:08:46 +0800

    day01

---
+++ src/main/scala/com/Location/ProCityCt.scala
@@ -0,0 +1,41 @@
+package com.Location
+
+import java.util.Properties
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
+object ProCityCt {
+  def main(args: Array[String]): Unit = {
+    if(args.length != 1){
+      println("输入文件不正确")
+      sys.exit()
+    }
+
+    val Array(inputPath) = args
+
+    val spark = SparkSession
+      .builder()
+      .appName("ct")
+      .master("local")
+      .config("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+
+    val df: DataFrame = spark.read.parquet(inputPath)
+    df.createTempView("log")
+    val df2 = spark.sql("select provincename,cityname,count(*) ct from log group by provincename,cityname")
+
+    //df2.write.partitionBy("provincename","cityname").parquet("H:\\ct")
+
+    val load: Config = ConfigFactory.load()
+    val prop = new Properties()
+    prop.setProperty("user",load.getString("jdbc.user"))
+    prop.setProperty("password",load.getString("jdbc.password"))
+    df2.write.mode(SaveMode.Append).jdbc(load.getString("jdbc.url"),
+      load.getString("jdbc.tableName"),
+      prop
+    )
+
+    spark.close()
+  }
+}
